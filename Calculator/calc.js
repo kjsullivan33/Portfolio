cc64cf2bd4d092c69bf53ddbb9504bc43c48d988
Kristen Sullivan <you@example.com>
2018-04-23 22:06:40 -0400

    fixed operator function and added equal button to calculate

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,70 +1,72 @@
 //To do: 
-//Display input plus operations into another line to keep track of multiple operations
-//Add an equal button
-//Fix the feature to continue operations without clearing the total
+//Calculate total from cumulated string of numbers and operators.
 
 var total = 0;
-var value = "0";
-var operator = "0";
+var value = 0;
+var str= 0;
+var myArray=[];
+var operator = 0;
 
 $(document).ready(function() {
   $("#display").text(value);
+  $("#string").text(str);
 });
 
-function setNum(num) {
-  if (value == "0") {
-    value = num;
-  } else {
-    value += num;
-  }
+function setNum(val) {
+    if (value === 0){
+        value = val;    
+    } else {
+    value += val;
+    } 
+    if (str === 0){
+        str = val;
+    } else {
+        str += val;
+    } 
   $("#display").text(value);
+  $("#string").text(str);
+
+
+
 }
 
 function addDec() {
   if (parseFloat(value) % 1 === 0) {
     var dec = ".";
     value += dec;
+    str += dec;
     $("#display").text(value);
+    $("#string").text(str);
   }
 }
 
 function setOper(oper) {
-  if (operator == "0"){
-      operator = oper;
-      total = parseFloat(value);
-      value = "0";
-      $("#display").text(value);
-          
-  } else {
-      calculate(operator);
-  }
+    str += oper;
+    $("#string").text(str);
+    
+    value = 0;
+    
+    
+  
 }
 
-function calculate(oper){
-  switch (oper) {
-    case "+":
-      total += total;
-      break;
-    case "-":
-      total -= total;
-      break;
-    case "*":
-      total *= total;
-      break;
-    case "/":
-      total /= total;
-      break;
-  }
-  $("#display").text(total);
-  operator = "0";
-  value = "0";
+
+function calculate(){
+    total = eval(str);
+    $("#display").text(total);
 }
 
+function resetVals(){
+    total = 0;
+    value = 0;
+    str = 0;
+    myArray = [];
+}
 
 function clearDisplay(){
-    total = 0;
-    value = "0";
+    resetVals();
     $("#display").text(value);
+    $("#string").text(str);
 }
 
 
