9f08e2b71bda71bebfadfd10c7e6ed05b6ad4c76
Kristen Sullivan <you@example.com>
2018-08-18 14:15:30 -0400

    refactored index.js for DRY code

--- index.js
+++ index.js
@@ -1,108 +1,40 @@
 window.onload = function () {
-  var aboutModal = document.getElementById('about');
-
-  var projectModal = document.getElementById('projects');
-
-  var projectBoxes = document.getElementById('projects-content');
-
-  var modalProject = document.getElementsByClassName("modal-project");
-
-  var aboutBtn = document.getElementById('btn-about');
+  var previousModal = document.getElementById("header");
+  var projectsModal = document.getElementById('projects-modal');
+  var btn = document.getElementsByClassName('click-to-open');
+  var closeBtn = document.getElementsByClassName('close');
   
 
-  var projectBtn = document.getElementById('btn-projects');
-
-  var weatherBtn = document.getElementById("btn-weather");
-
-  var flashBtn = document.getElementById("btn-flash");
-
-  var drumBtn = document.getElementById("btn-drum");
-
-  var markdownBtn = document.getElementById("btn-markdown");
-
-  var aboutSpan = document.getElementsByClassName("close")[0];
-
-  var projectSpan = document.getElementsByClassName("close")[1];
-
-  var weatherSpan = document.getElementsByClassName("back")[0];
-
-  var flashSpan = document.getElementsByClassName("back")[1];
-
-  var drumSpan = document.getElementsByClassName("back")[2];
-
-  var markdownSpan = document.getElementsByClassName("back")[3];
-
-  var header = document.getElementById("header");
-
-  
-
-
-
-  aboutBtn.onclick = function () {
-    aboutModal.style.display = "block";
-    header.style.display = "none";
+  //loop function for opening a modal and keeping track of which modal to hide
+  for (var i=0; i < btn.length; i++) {
+    var thisBtn = btn[i];
+    thisBtn.addEventListener("click", function() {
+      console.log(this.dataset.modal);
+      var nextModal = document.getElementById(this.dataset.modal);
+      console.log("nextModal: ", nextModal,"previousModal: ", previousModal);
+      nextModal.style.display="block";
+      previousModal.style.display="none";
+      previousModal = nextModal;
+      console.log("updatedPreviousModal: " ,previousModal);
+    })
   }
 
-  projectBtn.onclick = function () {
-    projectModal.style.display = "block";
-    header.style.display = "none";
-  }
-
-  weatherBtn.onclick = function () {
-    weather.style.display="block";
-    projectBoxes.style.display="none";
-  }
-
-  flashBtn.onclick = function () {
-    flash.style.display = "block";
-    projectBoxes.style.display = "none";
-  }
-
-  drumBtn.onclick = function () {
-    drum.style.display = "block";
-    projectBoxes.style.display = "none";
-  }
-
-  markdownBtn.onclick = function () {
-    markdown.style.display = "block";
-    projectBoxes.style.display = "none";
-  }
-
-  aboutSpan.onclick = function () {
-    aboutModal.style.display = "none";
-    header.style.display = "block";
-  }
-
-  projectSpan.onclick = function () {
-    projectModal.style.display = "none";
-    header.style.display = "block";
-  }
-
-  weatherSpan.onclick = function () {
-    weather.style.display="none";
-    projectBoxes.style.display="block";
-  }
-
-  flashSpan.onclick = function () {
-    flash.style.display = "none";
-    projectBoxes.style.display = "block";
-  }
-
-  drumSpan.onclick = function () {
-    drum.style.display = "none";
-    projectBoxes.style.display = "block";
-  }
-
-  markdownSpan.onclick = function () {
-    markdown.style.display = "none";
-    projectBoxes.style.display = "block";
-  }
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modalProject.style.zIndex = "2";
-      projectModal.style.display = "block";
-    }
+  //loop function for closing a modal and reassigning the previous modal to keep track of which modals 
+  //to be shown or hidden
+  for (var i=0; i < closeBtn.length; i++) {
+    var thisCloseBtn = closeBtn[i];
+    thisCloseBtn.addEventListener("click", function() {
+      var modal = document.getElementById(this.dataset.modal);
+      modal.style.display="none";
+      if (this.dataset.modal == "about-modal" || this.dataset.modal == "projects-modal"){
+        header.style.display = "block";
+        previousModal = document.getElementById("header");
+      } else {
+        projectsModal.style.display = "block";
+        previousModal = projectsModal;
+      }
+      
+    });
   }
 };
 
