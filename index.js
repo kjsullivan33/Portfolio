8df31e2d1a7c447423ac77aa5fe89d9b812923d1
Kristen Sullivan <you@example.com>
2018-08-18 11:31:10 -0400

    added project details modals, improved styling

--- index.js
+++ index.js
@@ -3,16 +3,40 @@ window.onload = function () {
 
   var projectModal = document.getElementById('projects');
 
+  var projectBoxes = document.getElementById('projects-content');
+
+  var modalProject = document.getElementsByClassName("modal-project");
+
   var aboutBtn = document.getElementById('btn-about');
-  console.log(aboutBtn);
+  
 
   var projectBtn = document.getElementById('btn-projects');
+
+  var weatherBtn = document.getElementById("btn-weather");
+
+  var flashBtn = document.getElementById("btn-flash");
+
+  var drumBtn = document.getElementById("btn-drum");
+
+  var markdownBtn = document.getElementById("btn-markdown");
 
   var aboutSpan = document.getElementsByClassName("close")[0];
 
   var projectSpan = document.getElementsByClassName("close")[1];
 
+  var weatherSpan = document.getElementsByClassName("back")[0];
+
+  var flashSpan = document.getElementsByClassName("back")[1];
+
+  var drumSpan = document.getElementsByClassName("back")[2];
+
+  var markdownSpan = document.getElementsByClassName("back")[3];
+
   var header = document.getElementById("header");
+
+  
+
+
 
   aboutBtn.onclick = function () {
     aboutModal.style.display = "block";
@@ -22,6 +46,26 @@ window.onload = function () {
   projectBtn.onclick = function () {
     projectModal.style.display = "block";
     header.style.display = "none";
+  }
+
+  weatherBtn.onclick = function () {
+    weather.style.display="block";
+    projectBoxes.style.display="none";
+  }
+
+  flashBtn.onclick = function () {
+    flash.style.display = "block";
+    projectBoxes.style.display = "none";
+  }
+
+  drumBtn.onclick = function () {
+    drum.style.display = "block";
+    projectBoxes.style.display = "none";
+  }
+
+  markdownBtn.onclick = function () {
+    markdown.style.display = "block";
+    projectBoxes.style.display = "none";
   }
 
   aboutSpan.onclick = function () {
@@ -34,7 +78,32 @@ window.onload = function () {
     header.style.display = "block";
   }
 
-};
+  weatherSpan.onclick = function () {
+    weather.style.display="none";
+    projectBoxes.style.display="block";
+  }
 
+  flashSpan.onclick = function () {
+    flash.style.display = "none";
+    projectBoxes.style.display = "block";
+  }
+
+  drumSpan.onclick = function () {
+    drum.style.display = "none";
+    projectBoxes.style.display = "block";
+  }
+
+  markdownSpan.onclick = function () {
+    markdown.style.display = "none";
+    projectBoxes.style.display = "block";
+  }
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modalProject.style.zIndex = "2";
+      projectModal.style.display = "block";
+    }
+  }
+};
 
 
